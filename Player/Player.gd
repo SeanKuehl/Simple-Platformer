6f83014449eca3c95203ed2c354cd781a47e63d1
Sean Kuehl <teddscottfelt@gmail.com>
2021-05-28 10:10:29 -0400

    changed level two, worked on level three and fixed bug where you could permenantly shift the camera by pressing multiple look buttons at the same time

--- Player/Player.gd
+++ Player/Player.gd
@@ -5,6 +5,10 @@ var jump_speed = -500
 var gravity = 800
 
 var buttonPressed = false
+var camDownPressed = false
+var camUpPressed = false
+var camLeftPressed = false
+var camRightPressed = false
 var cameraMoveAmount = 200
 
 var velocity = Vector2.ZERO
@@ -20,34 +24,43 @@ func get_input():
 		
 	#use CAM_UP, CAM_DOWN, CAM_LEFT, and CAM_RIGHT to "look" or move the camera
 	#$Camera2D.position -= transform.x * 10
-	if Input.is_action_pressed("CAM_DOWN") and buttonPressed == false:
+	if Input.is_action_pressed("CAM_DOWN") and camDownPressed == false and buttonPressed == false:
 		#$Camera2D.position += transform.x * 10	#this moves the camera right
 		$Camera2D.position += transform.y * cameraMoveAmount
+		camDownPressed = true
 		buttonPressed = true
-	if Input.is_action_just_released("CAM_DOWN"):
+	if Input.is_action_just_released("CAM_DOWN") and camDownPressed == true and buttonPressed == true:
 		$Camera2D.position -= transform.y * cameraMoveAmount
+		camDownPressed = false
 		buttonPressed = false
 		#if Input.is_action_just_released("click"):
 		
-	if Input.is_action_pressed("CAM_UP") and buttonPressed == false:
+	if Input.is_action_pressed("CAM_UP") and camUpPressed == false and buttonPressed == false:
+		
 		$Camera2D.position -= transform.y * cameraMoveAmount
+		camUpPressed = true
 		buttonPressed = true
-	if Input.is_action_just_released("CAM_UP"):
+	if Input.is_action_just_released("CAM_UP") and camUpPressed == true and buttonPressed == true:
 		$Camera2D.position += transform.y * cameraMoveAmount
+		camUpPressed = false
 		buttonPressed = false
 		
-	if Input.is_action_pressed("CAM_LEFT") and buttonPressed == false:
+	if Input.is_action_pressed("CAM_LEFT") and camLeftPressed == false and buttonPressed == false:
 		$Camera2D.position -= transform.x * cameraMoveAmount
+		camLeftPressed = true
 		buttonPressed = true
-	if Input.is_action_just_released("CAM_LEFT"):
+	if Input.is_action_just_released("CAM_LEFT") and camLeftPressed == true and buttonPressed == true:
 		$Camera2D.position += transform.x * cameraMoveAmount
+		camLeftPressed = false
 		buttonPressed = false
 		
-	if Input.is_action_pressed("CAM_RIGHT") and buttonPressed == false:
+	if Input.is_action_pressed("CAM_RIGHT") and camRightPressed == false and buttonPressed == false:
 		$Camera2D.position += transform.x * cameraMoveAmount
+		camRightPressed = true
 		buttonPressed = true
-	if Input.is_action_just_released("CAM_RIGHT"):
+	if Input.is_action_just_released("CAM_RIGHT") and camRightPressed == true and buttonPressed == true:
 		$Camera2D.position -= transform.x * cameraMoveAmount
+		camRightPressed = false
 		buttonPressed = false
 
 func _physics_process(delta):
